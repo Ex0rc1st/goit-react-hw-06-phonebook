7ef5fb7a37117b7f8001d902669dca3bcba6def7
Anton Salnykov <anton.salnikov2010@gmail.com>
2022-11-25 16:58:36 +0200

    add some code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,47 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'; // Импорт функции создания хранилища и прослойки
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'; // Импорт функции персистеров и фикса консоли
+import storage from 'redux-persist/lib/storage'; // Импорт локального хранилища из библиотеки персиста
+import logger from 'redux-logger'; // Импорт функции логгирования
+import contactsReducer from './contacts/contacts-reducer'; // Импорт редюсера по контактам
 
-// Начальное значение состояния Redux для корневого редюсера,
-// если не передать параметр preloadedState.
-const initialState = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
-  filter: {
-    status: '',
+// Конфиг персиста для контактов с блеклистом
+const contactsPersistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+
+// Создание прослоек + логгер. Важен порядок!
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+// Создание хранилища (корневой редюсер + прослойки + тулзы только для разработки)
+const store = configureStore({
+  reducer: {
+    contacts: persistReducer(contactsPersistConfig, contactsReducer),
   },
-};
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
+});
 
-// Пока что используем редюсер который
-// только возвращает полученное состояние
-const rootReducer = (state = initialState, action) => {
-  return state;
-};
+// Обёртка хранилища в персистор
+const persistor = persistStore(store);
 
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);
+// Экспорт хранилища и обёртки хранилища
+// eslint-disable-next-line
+export default { store, persistor };
