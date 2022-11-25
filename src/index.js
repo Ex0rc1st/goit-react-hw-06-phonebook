fb49eac975931543231db2ea8f6c7f6393ffb523
Anton Salnykov <anton.salnikov2010@gmail.com>
2022-11-25 17:24:20 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux'; // Импорт провайдера для хранилища
-import { PersistGate } from 'redux-persist/integration/react'; // Импорт гейта для персиста
-import store from './redux/store'; // Импорт обьекта из стора (хранилище и персистор)
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store from './redux/store';
 
 import App from './App.jsx';
 
